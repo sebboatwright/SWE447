af48bd409991b960ebe38758753757e297265b43
sebboatwright <84051000+sebboatwright@users.noreply.github.com>
2021-06-01 16:14:33 -0700

    Finished Assignment 4 Part 2

--- Common/Cube.js
+++ Common/Cube.js
@@ -85,7 +85,7 @@ function Cube( vertexShaderId, fragmentShaderId ) {
  
         gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
 
-        //gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
+        gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
 
         // Draw the cube's base
         gl.drawElements( gl.TRIANGLES, this.indices.count, gl.UNSIGNED_SHORT, 0 );
